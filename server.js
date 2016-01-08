e0622b0e52e6edf9eb52f1ea3c0ad0c652c5823a
Ian Goldsmith <ian.goldsmith@akana.com>
2016-01-08 18:29:35 +0000

    Major Update

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-var q = 'demo';
-
+var akana = {};
 var url = process.env.CLOUDAMQP_URL || "amqp://qlgmscns:kvcZM_64Xvkqzt1vawEwl3xOIwjdXPJQ@hyena.rmq.cloudamqp.com/qlgmscns";
 var open = require('amqplib').connect(url);
 
 // Consumer
 open.then(function(conn) {
-  var ok = conn.createChannel();
-  ok = ok.then(function(ch) {
-    ch.assertQueue(q);
-    ch.consume(q, function(msg) {
-      if (msg !== null) {
-        console.log(msg.content.toString());
-        ch.ack(msg);
+  conn.createChannel(function(err, ch) {
+    var q = 'demo';
+
+    ch.assertQueue(q, {durable: false});
+    ch.prefetch(1);
+    console.log(' [x] Awaiting RPC requests');
+    ch.consume(q, function reply(msg) {
+      console.log("Function Name: %s", msg.properties.headers.functionName);
+      
+      var r;
+      if (!akana[msg.properties.headers.functionName]) {
+        r = "Unknown function name";
+      } else {
+        r = akana[msg.properties.headers.functionName](msg.content);
       }
+      
+      ch.sendToQueue(msg.properties.replyTo,
+        new Buffer(r.toString()),
+        {correlationId: msg.properties.correlationId});
+
+      ch.ack(msg);
     });
   });
-  return ok;
-}).then(null, console.warn);
+});
 
-// Publisher
-open.then(function(conn) {
-  var ok = conn.createChannel();
-  ok = ok.then(function(ch) {
-    ch.assertQueue(q);
-    ch.sendToQueue(q, new Buffer('something to do'));
-  });
-  return ok;
-}).then(null, console.warn);
+akana.hello = function(n) {
+  return "Hello Akana asd!";
+};
+
+akana.reflect = function(n) {
+  return n;
+};
